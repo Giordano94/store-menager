c9e6fa0633ec81e48e54243447531b018c2754a7
Giordano <giordano.mendes94@gmail.com>
2023-01-17 19:07:42 -0300

    feat: test getProductsById

--- tests/unit/models/productsModels.test.js
+++ tests/unit/models/productsModels.test.js
@@ -7,13 +7,29 @@ const listProducts = require("./mocks/listProducts.model.mock");
 const { expect } = chai;
 
 describe("products Model", function () {
-  describe("products Model", function () {
-    it("Test if it returns all products in the list", async function () {
+  describe("Get all products", function () {
+    it("Check if it returns all products in the list", async function () {
+      afterEach(() => {
+        sinon.restore();
+      });
       sinon.stub(connection, "execute").resolves(listProducts);
 
       const result = await productsModel.getAllProducts();
 
       expect(result).to.be.deep.equal([listProducts]);
+    });
+  });
+
+  describe("List products by id", function () {
+    it("Check if the requested product is returned ", async function () {
+        afterEach(() => {
+          sinon.restore();
+        });
+      sinon.stub(connection, "execute").resolves([[listProducts[0]]]);
+
+      const result = await productsModel.getProductsById(1);
+
+      expect(result).to.be.deep.equal(listProducts[0]);
     });
   });
 });
