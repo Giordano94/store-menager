36ef1738a96cfb9c0f083c1745a4c9088aa676b8
Giordano <giordano.mendes94@gmail.com>
2023-01-17 19:11:31 -0300

    feat: service getAllProducts

---
+++ src/services/productsService.js
@@ -0,0 +1,10 @@
+const productsModel = require('../models/productsModel');
+
+const getAllProducts = async () => {
+  const allProducts = await productsModel.getAllProducts();
+  return allProducts;
+};
+
+module.exports = {
+  getAllProducts,
+};
