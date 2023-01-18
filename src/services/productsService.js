d16b9620a23e6c4e93a6013d50b912fa8aa8b567
Giordano <giordano.mendes94@gmail.com>
2023-01-18 16:39:02 -0300

    feat: service getProductById

--- src/services/productsService.js
+++ src/services/productsService.js
@@ -1,10 +1,21 @@
 const productsModel = require('../models/productsModel');
+const { validateId } = require('./validations/validationsInput');
 
 const getAllProducts = async () => {
   const allProducts = await productsModel.getAllProducts();
   return { type: null, message: allProducts };
 };
 
+const getProductById = async (productId) => {
+  const error = validateId(productId);
+  if (error.type) return error;
+
+  const product = await productsModel.getProductsById(productId);
+  if (product) return { type: null, message: product };
+  return { type: 'PRODUCT_NOT_FOUND', message: 'Product not found' };
+};
+
 module.exports = {
   getAllProducts,
+  getProductById,
 };
