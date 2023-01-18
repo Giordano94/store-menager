f7fc83296aaf7d909ba9955967b688e5439ddcd6
Giordano <giordano.mendes94@gmail.com>
2023-01-18 17:13:04 -0300

    feat: controller getProductById and refectoring getAllProducts

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,10 +1,20 @@
 const productsService = require('../services/productsService');
+const errorMap = require('../utils/errorMap');
 
 const getAllProducts = async (_req, res) => {
-  const allProducts = await productsService.getAllProducts();
-  res.status(200).json(allProducts);
+  const { type, message } = await productsService.getAllProducts();
+  if (type) return res.status(errorMap.mapError(type).json(message));
+  res.status(200).json(message);
+};
+
+const getProductById = async (req, res) => {
+  const { id } = req.params;
+  const { type, message } = await productsService.getProductById(id);
+  if (type) return res.status(errorMap.mapError(type).json(message));
+  return res.status(200).json(message);
 };
 
 module.exports = {
   getAllProducts,
+  getProductById,
 };
