04ecdce35303c2bb24f62d92444b48d8b63e5d42
Giordano <giordano.mendes94@gmail.com>
2023-01-22 16:55:01 -0300

    fix: Model sales

--- src/models/salesModels.js
+++ src/models/salesModels.js
@@ -2,6 +2,7 @@ const { connection } = require('./connection');
 
 const getAllSales = async () => {
   const [allSales] = await connection.execute(
+    // eslint-disable-next-line max-len
     'SELECT SlPr.sale_id, Sl.date, SlPr.product_id, SlPr.quantity FROM StoreManager.sales_products AS SlPr INNER JOIN StoreManager.sales AS Sl ON SlPr.sale_id = Sl.id ORDER BY sale_id, product_id ASC; ',
   );
   return allSales;
@@ -9,6 +10,7 @@ const getAllSales = async () => {
 
 const getSalesById = async (id) => {
   const [[saleId]] = await connection.execute(
+    // eslint-disable-next-line max-len
     ' SELECT Sl.date, SlPr.product_id, SlPr.quantity FROM StoreManager.sales_products AS SlPr INNER JOIN StoreManager.sales AS Sl ON SlPr.sale_id = Sl.id WHERE Sl.id = (?) ORDER BY sale_id , product_id ASC',
     [id],
   );
